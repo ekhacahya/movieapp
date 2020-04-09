397ffd9bd99c1f7d3679bfc1bfe221fbe9331321
Eka <ecbn95@gmail.com>
2020-04-10 01:16:56 +0300

    add video player

--- app/src/main/java/xyz/ecbn/moviemvvm/feature/PlayerActivity.kt
+++ app/src/main/java/xyz/ecbn/moviemvvm/feature/PlayerActivity.kt
@@ -1,21 +1,9 @@
 package xyz.ecbn.moviemvvm.feature
 
-import android.annotation.SuppressLint
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.dash.DashMediaSource
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import kotlinx.android.synthetic.main.activity_player.*
 import xyz.ecbn.moviemvvm.R
 
@@ -23,118 +11,21 @@ import xyz.ecbn.moviemvvm.R
 class PlayerActivity : AppCompatActivity() {
     private val TAG = PlayerActivity::class.java.name
 
-    var player: SimpleExoPlayer? = null
-    private lateinit var playbackStateListener: PlaybackStateListener
-    private var playWhenReady = true
-    private var currentWindow = 0
-    private var playbackPosition: Long = 0
     private var url: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
-        playbackStateListener = PlaybackStateListener()
+
         url = intent.getStringExtra("url")
-        url = "https://www.youtube.com/watch?v=$url"
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (Util.SDK_INT > 23) {
-            initPlayer()
-        }
-    }
-
-
-    override fun onResume() {
-        super.onResume()
-        hideSystemUi()
-        if (Util.SDK_INT <= 23 || player == null) {
-            initPlayer()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (Util.SDK_INT <= 23) {
-            releasePlayer()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        if (Util.SDK_INT > 23) {
-            releasePlayer()
-        }
-    }
-
-    private fun initPlayer() {
-        if (player == null) {
-            val trackSelector = DefaultTrackSelector()
-            trackSelector.setParameters(
-                trackSelector.buildUponParameters().setMaxVideoSizeSd()
-            )
-            player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
-        }
-
-        video_view.player = player
-//        val uri: Uri = Uri.parse(if (url != null) url else getString(R.string.media_url_dash))
-        val uri: Uri = Uri.parse("https://www.youtube.com/watch?v=NVoEDjvuhNI")
-//        val uri: Uri = Uri.parse(getString(R.string.media_url_dash))
-        val mediaSource: MediaSource = buildMediaSource(uri)
-
-        player?.playWhenReady = playWhenReady
-        player?.seekTo(currentWindow, playbackPosition)
-        player?.addListener(playbackStateListener)
-        player?.prepare(mediaSource, false, false)
-    }
-
-
-    private fun releasePlayer() {
-        if (player != null) {
-            playbackPosition = player?.currentPosition!!
-            currentWindow = player?.currentWindowIndex!!
-            playWhenReady = player?.playWhenReady!!
-            player?.removeListener(playbackStateListener)
-            player?.release()
-            player = null
-        }
-    }
-
-    private fun buildMediaSource(uri: Uri): MediaSource {
-        val dataSourceFactory: DataSource.Factory =
-            DefaultDataSourceFactory(this, "exoplayer-codelab")
-        val mediaSourceFactory = DashMediaSource.Factory(dataSourceFactory)
-
-        return mediaSourceFactory.createMediaSource(uri)
-    }
-
-    @SuppressLint("InlinedApi")
-    private fun hideSystemUi() {
-        video_view.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-    }
-
-    private class PlaybackStateListener : Player.EventListener {
-        override fun onPlayerStateChanged(
-            playWhenReady: Boolean,
-            playbackState: Int
-        ) {
-            val stateString = when (playbackState) {
-                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
-                ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
-                ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
-                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
-                else -> "UNKNOWN_STATE             -"
+        lifecycle.addObserver(youtubePlayer)
+        youtubePlayer.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                super.onReady(youTubePlayer)
+                youTubePlayer.loadVideo(url.toString(), 0F)
             }
-            Log.d(
-                "PLISterner", "changed state to " + stateString
-                        + " playWhenReady: " + playWhenReady
-            )
-        }
+        })
+
     }
+
 }
